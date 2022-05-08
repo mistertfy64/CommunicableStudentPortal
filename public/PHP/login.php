a657299d135950b1c17fd503bbeaa57229b81af2
Taweikung09 <taweikung09@gmail.com>
2022-05-08 10:18:01 +0700

    login fix

--- public/PHP/login.php
+++ public/PHP/login.php
@@ -1,32 +1,30 @@
  <?php
     session_start();
 
-    $username = $_POST[username];
-    $password = $_POST[password];
+    if($_GET[action]=="login"){
+
 
     $sql = "SELECT FROM [] WHERE username = '$username' AND password = '$password'";
+    $result= $connect->query($sql);
     $query = mysql_query($sql)or die("query error");
-    $userfound = mysql_num_rows($query);
+    $userfound = mysql_num_rows($result);
 
-    if ($userfound == 1){
-        $data = mysql_fetch_array($query);
-        $_SESSION[firstname] = $data[firstname];
-	      $_SESSION[lastname] = $data[lastname];
-	      $_SESSION[username] = $data[username];
-	      $_SESSION[email] = $data[email];
-        $_SESSION[memebrID] = $data[memberID];
-	      $_SESSION[tel] = $data[tel];
-        $_SESSION[year] = $data[year];
-        $_SESSION[level] = $data[level];
-        echo"<script>
-			setTimeout(\"window.location.href='/'\",3000);
-		</script>";
-    }else{
-        session_abort();
-        echo"Login failed!";
-        echo"<script>
-			setTimeout(\"window.location.href='/'\",3000);
-		</script>";
+      if($userfound == 1){
+        $row = $result->fetch_asoc();
+
+        $_SESSION[firstname] = $row[firstname];
+	      $_SESSION[lastname] = $row[lastname];
+	      $_SESSION[username] = $row[username];
+	      $_SESSION[email] = $row[email];
+        $_SESSION[memebrID] = $row[memberID];
+	      $_SESSION[tel] = $row[tel];
+        $_SESSION[year] = $row[year];
+        $_SESSION[level] = $row[level];
+
+        header('location: index.ejs');
+      }else{
+        
+      }
     }
 
 ?>
