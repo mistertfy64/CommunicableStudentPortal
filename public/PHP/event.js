849c17998c7183fe04903bcabe91b28d2c32102e
Taweikung09 <taweikung09@gmail.com>
2022-05-13 10:35:12 +0700

    a

--- public/PHP/event.js
+++ public/PHP/event.js
@@ -3,16 +3,27 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:8888/Cluster1');
+  await mongoose.connect('mongodb://localhost:22222/Cluster1');
 }
 
 const eventSchema = new mongoose.Schema({
-    name: String
+    title: String,
+    datetime: { type: Date, default: Date.now },
+    author: String,
+    describtion: String,
+    comment: [{ body: String, datetime: Date }],
   });
 
 const Event = mongoose.model('Event', eventSchema);
 
-const silence = new Event({ name: 'Lecture' });
+const silence = new Event({
+   title: 'Lecture',
+   datetime: '2022-06-15T14:00:00 GMT-0700',
+   author: 'Jane Doe',
+   describtion: 'On the 5th of June there will be a lecture about the ecosystem.',
+   comment: [{}],
+});
+
 console.log(lecture.name);
 
 eventSchema.methods.speak = function speak() {
@@ -22,12 +33,10 @@ eventSchema.methods.speak = function speak() {
     console.log(eventTitle);
 };
   
-const Kitten = mongoose.model('Kitten', kittySchema);
-  
 const orientation = new Event({ name: 'Orientation' });
 orientation.speak();
 
 const events = await Event.find();
 console.log(events);
 
-await Kitten.find({ name: /^orientation/ });
+await Event.find({ name: /^orientation/ });
