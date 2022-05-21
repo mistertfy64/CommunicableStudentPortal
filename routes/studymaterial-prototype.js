f35e21e49f2806eabf7347e32d5e749811f56085
Taweikung09 <taweikung09@gmail.com>
2022-05-21 10:13:44 +0700

    renaming stuff

--- routes/studymaterial-prototype.js
+++
@@ -1,8 +0,0 @@
-var router = require("express").Router();
-const configuration = require("../configuration.js");
-
-router.get("/studymaterial-prototype", (request, response) => {
-    response.render("pages/studymaterial-prototype", {configuration: configuration.safeConfiguration}); 
-});
-
-module.exports = router;
