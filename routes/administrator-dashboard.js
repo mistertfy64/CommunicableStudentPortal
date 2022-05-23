24ba2f98c34b1bbb9b8d05467f11bb2018613b6b
Tinnaphop Choonchuachan <53388199+mistertfy64@users.noreply.github.com>
2022-05-23 19:29:27 +0700

    admin dashboard part 1

--- routes/administrator-dashboard.js
+++ routes/administrator-dashboard.js
@@ -2,7 +2,7 @@ var router = require("express").Router();
 const configuration = require("../configuration.js");
 
 router.get("/administrator-dashboard", (request, response) => {
-    response.render("pages/administrator-dashboard", {configuration: configuration.safeConfiguration}); 
+    response.render("pages/administrator/administrator-dashboard", {configuration: configuration.safeConfiguration}); 
 });
 
 module.exports = router;
