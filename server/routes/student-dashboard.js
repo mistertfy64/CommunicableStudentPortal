b56b73e68a960d3216aba750996292768048502f
Tinnaphop Choonchuachan <53388199+mistertfy64@users.noreply.github.com>
2022-06-01 20:02:27 +0700

    change: removed the slideshow dots (use the arrows)

--- server/routes/student-dashboard.js
+++ server/routes/student-dashboard.js
@@ -2,7 +2,7 @@ var router = require("express").Router();
 const configuration = require("../configuration.js");
 
 router.get("/student-dashboard", (request, response) => {
-    response.render("pages/student-dashboard", {configuration: configuration.safeConfiguration}); 
+    response.render("pages/user/student-dashboard", {configuration: configuration.safeConfiguration}); 
 });
 
 module.exports = router;
