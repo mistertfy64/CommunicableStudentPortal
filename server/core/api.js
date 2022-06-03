041c06a0a5ade5ec580cfe6fddf635b25706da6c
Tinnaphop Choonchuachan <53388199+mistertfy64@users.noreply.github.com>
2022-06-03 14:44:57 +0700

    feature: http api (INCOMPLETE)

--- server/core/api.js
+++ server/core/api.js
@@ -3,16 +3,37 @@ const configuration = require("../configuration.js");
 const url = require("url");
 const User = require("../models/User.js");
 
-router.get("/users/:userID", (request, response) => {
-    let safe = request.query.safe;
-    if (false){}
-    
-    if (!(safe === true || safe === false)){
-        res.status(400).send("Error 400");
+router.get("/api/users/:userID", async (request, response) => {
+
+    if (!request.headers["x-api-key"]){
+        response.status(400).send("Error 400");
         return;
     }
+    let apiKey = request.headers["x-api-key"];
+    //FIXME: Consider data validation.
+    let userCalling = await User.safeFindUserByAPIKey(apiKey);
+
+    if (!userCalling){
+        response.status(400).send("Error 400");
+        return;
+    }
+
+    if (!(userCalling.membership.isAdministrator || userCalling.membership.isSuperAdministrator)){
+        response.status(400).send("Error 400");
+        return;
+    }
+
+    let safe = request.query.safe;
+    
+    if (!(safe === "true" || safe === "false")){
+        response.status(400).send("Error 400");
+        return;
+    }
+
     if (safe){
-        User.safeFindUserByUserID(request.params.userID)
+        let data = await User.safeFindUserByUserID(request.params.userID)
+        response.status(200).send(data);
+        return;
     } else {
 
     }
