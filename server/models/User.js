16799da4686d11877a84e1377a2a6cb33124406a
Tinnaphop Choonchuachan <53388199+mistertfy64@users.noreply.github.com>
2022-06-04 14:18:21 +0700

    feature: normal user usernames can now be changed

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const { SHA3 }= require("sha3");
+const { SHA3 } = require("sha3");
 const log = require("../core/log.js");
-
 
 const UserSchema = new mongoose.Schema({
 	name: String,
@@ -22,103 +21,141 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.statics.safeFindUserByUsername = function (username) {
-	return this.findOne({ username: username }).select({sessionTokens: 0, sessionTokensWithExpiryTime: 0, password: 0, apiKey: 0});
+	return this.findOne({ username: username }).select({
+		sessionTokens: 0,
+		sessionTokensWithExpiryTime: 0,
+		password: 0,
+		apiKey: 0,
+	});
 };
 
 UserSchema.statics.safeFindUserByUserID = function (userID) {
-	return this.findOne({ userID: userID }).select({sessionTokens: 0, sessionTokensWithExpiryTime: 0, password: 0, apiKey: 0});
+	return this.findOne({ userID: userID }).select({
+		sessionTokens: 0,
+		sessionTokensWithExpiryTime: 0,
+		password: 0,
+		apiKey: 0,
+	});
 };
 
 UserSchema.statics.destroyAllSessionTokensForUserID = function (userID) {
 	this.findOneAndUpdate(
 		{ userID: userID },
-		{ $set: { sessionTokens: [] }} ,((error, result) => {
-		// error?
-		if (error) {
-			console.error("Unable to destroy session tokens");
-			console.error(error.stack);
+		{ $set: { sessionTokens: [] } },
+		(error, result) => {
+			// error?
+			if (error) {
+				console.error("Unable to destroy session tokens");
+				console.error(error.stack);
+			}
 		}
-	}));
+	);
 };
 
 UserSchema.statics.addSessionTokenForUserID = function (userID, token) {
 	this.findOneAndUpdate(
 		{ userID: userID },
-		{ $push: { sessionTokens: token }},((error, result) => {
-		// error?
-		if (error) {
-			console.error("Unable to generate a session id");
-			console.error(`Error stack: ${error}`);
+		{ $push: { sessionTokens: token } },
+		(error, result) => {
+			// error?
+			if (error) {
+				console.error("Unable to generate a session id");
+				console.error(`Error stack: ${error}`);
+			}
 		}
-	}));
+	);
 };
 
-UserSchema.statics.safeFindUserBySessionToken = function(token){
-
-	if (!token){
+UserSchema.statics.safeFindUserBySessionToken = function (token) {
+	if (!token) {
 		console.error(log.addMetadata(`Token is undefined`, "error"));
 		return;
 	}
 
 	let hash = new SHA3(512);
-	hash.update(token.toString())
+	hash.update(token.toString());
 	let hashDigest = hash.digest("hex");
-	return this.findOne({sessionTokens: {$all:[hashDigest]}}).select({sessionTokens: 0, sessionTokensWithExpiryTime: 0, password: 0, apiKey: 0});
-}
+	return this.findOne({ sessionTokens: { $all: [hashDigest] } }).select({
+		sessionTokens: 0,
+		sessionTokensWithExpiryTime: 0,
+		password: 0,
+		apiKey: 0,
+	});
+};
 
-
-UserSchema.statics.findUserBySessionToken = function(token){
-	if (!token){
+UserSchema.statics.findUserBySessionToken = function (token) {
+	if (!token) {
 		console.error(log.addMetadata(`Token is undefined`, "error"));
 		return;
 	}
-	
 
 	let hash = new SHA3(512);
-	hash.update(token.toString())
+	hash.update(token.toString());
 	let hashDigest = hash.digest("hex");
-	return this.findOne({sessionTokens:{$all:[hashDigest]}});
-}
+	return this.findOne({ sessionTokens: { $all: [hashDigest] } });
+};
 
-UserSchema.statics.createAPIKeyForUserID = function(userID, key){
+UserSchema.statics.createAPIKeyForUserID = function (userID, key) {
 	let hash = new SHA3(512);
-	hash.update(key.toString())
+	hash.update(key.toString());
 	let hashDigest = hash.digest("hex");
 	return this.findOneAndUpdate(
 		{ userID: userID },
-		{ $set: { apiKey: hashDigest }} ,((error, result) => {
-		// error?
-		if (error) {
-			//TODO: error message
+		{ $set: { apiKey: hashDigest } },
+		(error, result) => {
+			// error?
+			if (error) {
+				//TODO: error message
+			}
 		}
-	}));
-}
+	);
+};
 
-UserSchema.statics.safeFindUserByAPIKey = function(apiKey){
-
-	if (!apiKey){
+UserSchema.statics.safeFindUserByAPIKey = function (apiKey) {
+	if (!apiKey) {
 		console.error(log.addMetadata(`API key is undefined.`, "error"));
 		return;
 	}
-	
-	let hash = new SHA3(512);
-	hash.update(apiKey.toString())
-	let hashDigest = hash.digest("hex");
-	return this.findOne({apiKey: hashDigest}).select({sessionTokens: 0, sessionTokensWithExpiryTime: 0, password: 0, apiKey: 0});
-}
 
-UserSchema.statics.findUserByAPIKey = function(apiKey){
-	
-	if (!apiKey){
+	let hash = new SHA3(512);
+	hash.update(apiKey.toString());
+	let hashDigest = hash.digest("hex");
+	return this.findOne({ apiKey: hashDigest }).select({
+		sessionTokens: 0,
+		sessionTokensWithExpiryTime: 0,
+		password: 0,
+		apiKey: 0,
+	});
+};
+
+UserSchema.statics.findUserByAPIKey = function (apiKey) {
+	if (!apiKey) {
 		console.error(log.addMetadata(`API key is undefined.`, "error"));
 		return;
 	}
-	
-	
+
 	let hash = new SHA3(512);
-	hash.update(apiKey.toString())
+	hash.update(apiKey.toString());
 	let hashDigest = hash.digest("hex");
-	return this.findOne({apiKey: hashDigest});
-}
+	return this.findOne({ apiKey: hashDigest });
+};
+
+// ===========================================
+
+UserSchema.statics.changeUsernameForUserID = function (userID, newUsername) {
+	// FIXME: This does not check for a user with already existing username. 
+	// Therefore, when calling this, make sure to have some duplicate username checking.
+	this.findOneAndUpdate(
+		{ userID: userID },
+		{ $set: { username: newUsername } },
+		(error, result) => {
+			// error?
+			if (error) {
+				console.error("Unable to change username");
+				console.error(`Error stack: ${error}`);
+			}
+		}
+	);
+};
 
 module.exports = mongoose.model("User", UserSchema, "users");
--- server/routes/student-dashboard.js
+++ server/routes/student-dashboard.js
@@ -1,8 +1,13 @@
 var router = require("express").Router();
 const configuration = require("../configuration.js");
+const cookieParser = require("cookie-parser");
+const User = require("../models/User.js");
 
-router.get("/student-dashboard", (request, response) => {
-    response.render("pages/user/student-dashboard", {configuration: configuration.safeConfiguration}); 
+router.use(cookieParser());
+
+router.get("/student-dashboard", async (request, response) => {
+    let data = await User.safeFindUserBySessionToken(request.cookies.sessionToken);
+    response.render("pages/user/student-dashboard", {configuration: configuration.safeConfiguration, data: data}); 
 });
 
 module.exports = router;
