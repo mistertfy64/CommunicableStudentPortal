8dd459255ad223c8f6607c129beae6a7da675f5e
Tinnaphop Choonchuachan <53388199+mistertfy64@users.noreply.github.com>
2022-06-06 16:43:21 +0700

    change: chat page now requires user to be logged in

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,8 +1,18 @@
 var router = require("express").Router();
 const configuration = require("../configuration.js");
+const User = require("../models/User.js");
 
-router.get("/chat", (request, response) => {
+router.get("/chat", async (request, response) => {
+    
+    let sessionToken = request.cookies.sessionToken;
+	let currentUser = await User.safeFindUserBySessionToken(sessionToken);
+	if (!currentUser) {
+		response.redirect("/");
+		return;
+	}
+
     response.render("pages/chat", {configuration: configuration.safeConfiguration}); 
+
 });
 
 module.exports = router;
