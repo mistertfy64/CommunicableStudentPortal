9d2a383592fe9341376b418b927467ab356eba03
Tinnaphop Choonchuachan <53388199+mistertfy64@users.noreply.github.com>
2022-06-11 19:44:22 +0700

    feature: leaderboard now sorts

--- server/routes/leaderboard.js
+++ server/routes/leaderboard.js
@@ -4,14 +4,14 @@ const User = require("../models/User.js");
 
 
 router.get("/leaderboard", async (request, response) => {
-    response.render("pages/leaderboard", {configuration: configuration.safeConfiguration, data: []}); 
+    response.render("pages/leaderboard", {configuration: configuration.safeConfiguration, data: [], stat: undefined}); 
 });
 
 
 router.get("/leaderboard/:stat", async (request, response) => {
     let statToGet = request.params.stat;
     let data = await User.getStatValueForLeaderboards(statToGet);
-    console.log(data);
+    data = data.sort((first, second) => parseFloat(second.statistics[statToGet]) - parseFloat(first.statistics[statToGet]));
     response.render("pages/leaderboard", {configuration: configuration.safeConfiguration, data: data, stat: statToGet}); 
 });
 
